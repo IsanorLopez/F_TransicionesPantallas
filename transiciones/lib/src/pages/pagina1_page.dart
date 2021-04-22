ff20f2f63a30aa12a5bc718491f7ac1715312b56
Isanor Alejandro Lopez Molina <isanoralejandro@gmail.com>
2021-04-22 10:46:21 -0600

    F_TransicionesPantallas[ - RotationTransition ]

--- transiciones/lib/src/pages/pagina1_page.dart
+++ transiciones/lib/src/pages/pagina1_page.dart
@@ -29,18 +29,23 @@ class Pagina1Page extends StatelessWidget {
       transitionDuration: Duration(seconds: 1),
       transitionsBuilder: ( context, animation, secondaryAnimation, child ) {
       
-        final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.ease);
+        final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInCubic);
 
         //return SlideTransition(
         //  position: Tween<Offset>( begin: Offset(0.5, 1.0), end: Offset.zero ).animate(curvedAnimation),
         //  child: child,
         //);
         
-        return ScaleTransition(
-          child: child,
-          scale: Tween<double>( begin: 0.0, end: 1.0).animate(curvedAnimation)
-        );
+        //return ScaleTransition(
+        //  child: child,
+        //  scale: Tween<double>( begin: 0.0, end: 1.0).animate(curvedAnimation)
+        //);
         
+
+        return RotationTransition(
+          child: child,
+          turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation)
+        );
 
       }
     );   
